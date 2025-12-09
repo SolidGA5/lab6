8624874f3c4c1efb63282956e78a02c697f977a5
SolidGA5 <phuphing.gh.chompubang@gmail.com>
2025-12-09 16:04:00 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+double deg2rad(double x){
+    return (M_PI/180.0)*x;
+}
+double rad2deg(double x){
+    return (180.0/M_PI)*x;
+}
+double findXComponent(double l1 , double l2 , double a1, double a2){
+    return l1*cos(a1)+l2*cos(a2);
+}
+double findYComponent(double l1 , double l2 , double a1, double a2){
+    return l1*sin(a1)+l2*sin(a2);
+}
+double pythagoras(double xcomp ,double ycomp){
+    return sqrt(pow(xcomp,2)+pow(ycomp,2));
+}
+void showResult(double result_vec_length, double result_vec_direction){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << "\n";
+    cout << "Length of the resultant vector = "<< result_vec_length << "\n";
+    cout << "Direction of the resultant vector (deg) = " << result_vec_direction << "\n";
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
+
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
@@ -8,7 +31,7 @@ int main(){
     cin >> l1;
     cout << "Enter direction of the first vector (deg): ";
     cin >> a1;
-    cout << "Enter length of the second vector: ";
+        cout << "Enter length of the second vector: ";
     cin >> l2;
     cout << "Enter direction of the second vector (deg): ";
     cin >> a2;
